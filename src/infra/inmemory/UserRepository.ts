b1ae1fe23e4e206f87afebb7e4edaea03ea49579
MAgungHKM <maranga12341@yahoo.com>
2022-11-25 01:40:07 +0700

    Setup in memory infrastructure

---
+++ src/infra/inmemory/UserRepository.ts
@@ -0,0 +1,50 @@
+import { ERROR_NOT_FOUND } from "./db";
+import { IUserRepository, User, UserNotFound } from "../../core/users";
+import { InMemoryDB } from "./db";
+
+export class InMemoryUserRepository implements IUserRepository {
+  db = InMemoryDB.getInstance();
+
+  getAll = () => {
+    const users = Object.values(this.db.users().getAll());
+
+    return { users };
+  };
+
+  getById = (userId: number) => {
+    const { user, error } = this.db.users().getById(userId);
+    if (error) {
+      if (error === ERROR_NOT_FOUND) return { error: new UserNotFound(userId) };
+      else return { error: { message: error } };
+    }
+
+    return { user };
+  };
+
+  create = (user: User) => {
+    this.db.users().create(user);
+
+    return undefined;
+  };
+
+  update = (user: User) => {
+    const { user: updatedUser, error } = this.db.users().update(user);
+    if (error) {
+      if (error === ERROR_NOT_FOUND && user._id)
+        return { error: new UserNotFound(user._id) };
+      else return { error: { message: error } };
+    }
+
+    return { user: updatedUser };
+  };
+
+  delete = (userId: number) => {
+    const { user, error } = this.db.users().delete(userId);
+    if (error) {
+      if (error === ERROR_NOT_FOUND) return { error: new UserNotFound(userId) };
+      else return { error: { message: error } };
+    }
+
+    return { user };
+  };
+}
---
+++ src/infra/inmemory/db.ts
@@ -0,0 +1,57 @@
+import { User } from "../../core/users";
+
+export const ERROR_NOT_FOUND = "Not found";
+
+export class InMemoryDB {
+  private static instance: InMemoryDB;
+  private user = {
+    map: {} as Record<number, User>,
+    lastInsertedId: 1,
+  };
+
+  private constructor() {}
+
+  static getInstance(): InMemoryDB {
+    if (!InMemoryDB.instance) {
+      InMemoryDB.instance = new InMemoryDB();
+    }
+
+    return InMemoryDB.instance;
+  }
+
+  users = () => ({
+    getAll: () => this.user.map,
+    getById: (userId: number) => {
+      const user = this.user.map[userId];
+      if (!user) {
+        return { error: ERROR_NOT_FOUND };
+      }
+
+      return { user };
+    },
+    create: (user: User) => {
+      const userId = this.user.lastInsertedId++;
+      user._id = userId;
+      this.user.map[userId] = user;
+    },
+    update: (user: User) => {
+      if (!user._id || !this.user.map[user._id]) {
+        return { error: ERROR_NOT_FOUND };
+      }
+
+      this.user.map[user._id] = user;
+
+      return { user: this.user.map[user._id] };
+    },
+    delete: (userId: number) => {
+      const user = this.user.map[userId];
+      if (!user) {
+        return { error: ERROR_NOT_FOUND };
+      }
+
+      delete this.user.map[userId];
+
+      return { user };
+    },
+  });
+}

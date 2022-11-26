433cb1730ed3d25b0b6d33487de831ee9e5d5efb
MAgungHKM <maranga12341@yahoo.com>
2022-11-26 18:52:51 +0700

    Setup fastify plugin for prisma

---
+++ src/plugins/prisma.ts
@@ -0,0 +1,28 @@
+import fp from "fastify-plugin";
+import { PrismaClient } from "@prisma/client";
+
+export interface PrismaPluginOptions {
+  // Specify Boot plugin options here
+}
+
+// The use of fastify-plugin is required to be able
+// to export the decorators to the outer scope
+export default fp<PrismaPluginOptions>(async (server, options) => {
+  const prisma = new PrismaClient();
+
+  await prisma.$connect();
+
+  // Make Prisma Client available through the fastify server instance: server.prisma
+  server.decorate("prisma", prisma);
+
+  server.addHook("onClose", async (server) => {
+    await server.prisma.$disconnect();
+  });
+});
+
+// Use TypeScript module augmentation to declare the type of server.prisma to be PrismaClient
+declare module "fastify" {
+  export interface FastifyInstance {
+    prisma: PrismaClient;
+  }
+}
